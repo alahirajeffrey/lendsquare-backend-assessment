4500ad85023cc43afbcb228a358c9bf3ab2b6895
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-12 19:41:32 +0100

    passport config file

---
+++ src/config/passport.config.ts
@@ -0,0 +1,22 @@
+// import prisma from "./prisma.config";
+// import { ExtractJwt, Strategy } from "passport-jwt";
+// import passport from "passport";
+
+// const ACCESS_TOKEN_SECRET = process.env.JWT_SECRET || "access_secret";
+
+// const opts = {
+//   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+//   secretOrKey: ACCESS_TOKEN_SECRET,
+// };
+
+// // setup jwt passport strategy
+// passport.use(
+//   new Strategy(opts, async (jwtPayload, done) => {
+//     const user = await prisma.user.findFirstOrThrow({
+//       where: { id: jwtPayload.id },
+//     });
+//     if (user) {
+//       return done(null, user);
+//     }
+//   })
+// );

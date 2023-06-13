b6aba0e52d8a2b559cbe11b02a76a8cd690df198
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-13 11:44:32 +0100

    setup wallet and transaction router

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from "express";
 import limiter from "./middlewares/rateLimiter.middleware";
 import authRouter from "./routes/auth.route";
+import walletRouter from "./routes/wallet.route";
+import transactionRouter from "./routes/transaction.route";
 
 const server: Express = express();
 
@@ -8,7 +10,9 @@ server.use(express.json());
 server.use(limiter);
 
 // setup routes here
-server.use("/api/v1", authRouter);
+server.use("/api/v1/auth", authRouter);
+server.use("/api/v1/wallet", walletRouter);
+server.use("/api/v1/transaction", transactionRouter);
 
 server.get("/api/v1/", (req: Request, res: Response) => {
   return res.status(200).json({ message: "welcome" });

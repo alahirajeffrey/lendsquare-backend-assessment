0a87a9c4df8522534edacade6d03c707b85bbf4b
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-13 11:09:56 +0100

    refactor

--- src/config/knex.config.ts
+++ src/config/knex.config.ts
@@ -6,10 +6,10 @@ module.exports = {
   development: {
     client: "mysql2",
     connection: {
-      host: process.env.MYSQL_HOST,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
+      host: process.env.MYSQL_HOST || "localhost",
+      user: process.env.MYSQL_USER || "mysql",
+      password: process.env.MYSQL_PASSWORD || "password",
+      database: process.env.MYSQL_DATABASE || "test",
     },
     migrations: {
       directory: "./src/database/migrations",
--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,12 +1,15 @@
 import knex from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = knex({
   client: "mysql2",
   connection: {
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST || "localhost",
+    user: process.env.MYSQL_USER || "mysql",
+    password: process.env.MYSQL_PASSWORD || "password",
+    database: process.env.MYSQL_DATABASE || "test",
   },
 });
 

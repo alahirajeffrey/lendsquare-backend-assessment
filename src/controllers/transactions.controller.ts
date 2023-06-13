69f0bbefb741ec9a34ec2b19947c87ae7c918168
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-13 13:04:06 +0100

    completed controller to fund account

--- src/controllers/transactions.controller.ts
+++ src/controllers/transactions.controller.ts
@@ -1,19 +1,56 @@
 import { StatusCodes } from "http-status-codes";
 import { Request, Response } from "express";
+import db from "../knexfile";
+import { ApiResponse } from "../types/response.type";
 
 /**
- * refresh access token
+ * fund user account
  * @param req
  * @param res
  */
-export const fundAccount = async (req: Request, res: Response) => {
-  res.status(StatusCodes.OK).json({ message: "account funded" });
+export const fundAccount = async (
+  req: Request,
+  res: Response
+): Promise<Response<ApiResponse>> => {
+  try {
+    const walletId = req.params.walletId;
+    const { amount } = req.body;
+
+    // check if wallet exists
+    const [walletExists] = await db("wallets").where({ walletId: walletId });
+    if (!walletExists) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "you cannot fund an account that does not exitst" });
+    }
+
+    // fund account
+    await db("wallets")
+      .where({ walletId: walletId })
+      .update({ balance: amount });
+
+    return res.status(StatusCodes.OK).json({ message: "account funded" });
+  } catch (error: any) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
+  }
 };
 
+/**
+ *
+ * @param req
+ * @param res
+ */
 export const transferFunds = async (req: Request, res: Response) => {
   res.status(StatusCodes.OK).json({ message: "transfer successful" });
 };
 
+/**
+ *
+ * @param req
+ * @param res
+ */
 export const withdrawal = async (req: Request, res: Response) => {
   res.status(StatusCodes.OK).json({ message: "withdrawal successful" });
 };

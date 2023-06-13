b4b462fb704a1bea1fef9b4043f61c19c438bcaf
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-13 11:01:35 +0100

    refactor

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -3,10 +3,10 @@ import knex from "knex";
 const db = knex({
   client: "mysql2",
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
   },
 });
 

d31f826490469679ac869eb1927399e7affae210
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-14 14:28:00 +0100

    fixed bug: return object user if authorized

--- src/config/passport.config.ts
+++ src/config/passport.config.ts
@@ -11,13 +11,21 @@ const jwtOptions = {
 
 const jwtStrategy = new Strategy(jwtOptions, async (payload, done) => {
   try {
-    // Find the user based on the user id
-    const user = await db("users").where("id", payload.sub).first();
+    // Find the user based on the user id from the payload
+    const user = await db
+      .queryBuilder()
+      .select()
+      .from("users")
+      .where({ id: payload.id })
+      .first();
 
     // return false if user is not founf
     if (!user) {
       return done(null, false);
     }
+
+    // return user if payload is correct
+    return done(null, user);
   } catch (error) {
     return done(error, false);
   }

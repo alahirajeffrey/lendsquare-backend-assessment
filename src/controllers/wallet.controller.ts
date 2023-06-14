996e259f9f8a2a96569c2208f35305dbeb19ecea
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-14 14:29:12 +0100

    refactored code

--- src/controllers/wallet.controller.ts
+++ src/controllers/wallet.controller.ts
@@ -2,7 +2,22 @@ import { StatusCodes } from "http-status-codes";
 import { Request, Response } from "express";
 import { ApiResponse } from "../types/response.type";
 import db from "../knexfile";
+import { Wallet } from "../types/wallet.type";
+import * as uuid from "uuid";
 
+/**
+ * checks if a user already has a wallet
+ * @param userId : string
+ * @returns : null or wallet details
+ */
+const checkWalletExists = async (userId: string): Promise<Wallet> => {
+  return await db
+    .queryBuilder()
+    .select()
+    .from("wallets")
+    .where({ userId: userId })
+    .first();
+};
 /**
  * create a wallet for user
  * @param req : request object containing userId
@@ -14,10 +29,9 @@ export const createWallet = async (
 ): Promise<Response<ApiResponse>> => {
   try {
     const { userId } = req.body;
-    // check if wallet already exists
-    const [walletExists] = await db("wallets")
-      .where({ userId: userId })
-      .first();
+
+    // ensure only one wallet can be created per user
+    const walletExists = await checkWalletExists(userId);
     if (walletExists) {
       return res
         .status(StatusCodes.BAD_REQUEST)
@@ -25,9 +39,20 @@ export const createWallet = async (
     }
 
     // create wallet
-    await db("wallets").insert({ userId: userId, balance: 0 });
+    const walletId = uuid.v4();
+    await db("wallets").insert({
+      userId: userId,
+      balance: 0,
+      id: walletId,
+    });
 
-    return res.status(StatusCodes.OK).json({ message: "wallet created" });
+    return res.status(StatusCodes.OK).json({
+      message: "wallet created",
+      data: {
+        walletId: walletId,
+        balance: 0,
+      },
+    });
   } catch (error: any) {
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)

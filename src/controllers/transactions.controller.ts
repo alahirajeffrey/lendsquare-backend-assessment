be4ae8c10c06b3747ab486acb5e89d5c75898c2f
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-15 10:06:10 +0100

    added controller to get a wallet's transactions

--- src/controllers/transactions.controller.ts
+++ src/controllers/transactions.controller.ts
@@ -4,6 +4,48 @@ import db from "../knexfile";
 import { ApiResponse } from "../types/response.type";
 
 /**
+ * get all a wallet's transactions
+ * @param req : request object with walletId as params
+ * @param res : response object
+ */
+export const getWalletTransactions = async (
+  req: Request,
+  res: Response
+): Promise<Response<ApiResponse>> => {
+  try {
+    const { walletId } = req.params;
+
+    // Check to see if wallet extists
+    const walletExists = await db
+      .queryBuilder()
+      .select()
+      .from("wallets")
+      .where({ id: walletId })
+      .first();
+
+    if (!walletExists) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "wallet does not exist" });
+    }
+
+    // get transactions
+    const transactions = await db
+      .queryBuilder()
+      .select()
+      .from("transactions")
+      .where({ senderWalletId: walletId });
+
+    return res
+      .status(StatusCodes.OK)
+      .json({ message: "transaction details", data: { transactions } });
+  } catch (error: any) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
+  }
+};
+/**
  * fund user account
  * @param req
  * @param res

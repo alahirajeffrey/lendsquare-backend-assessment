fbeebad6fd4709c5a98eba63f4ce529b14d14fc7
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-15 09:51:41 +0100

    route to get wallet details with userId

--- src/routes/wallet.route.ts
+++ src/routes/wallet.route.ts
@@ -2,8 +2,14 @@ import { Router } from "express";
 import { validate } from "express-validation";
 import passport from "passport";
 import "../config/passport.config";
-import { createWallet } from "../controllers/wallet.controller";
-import { validateCreateWallet } from "../middlewares/validations.middleware";
+import {
+  createWallet,
+  getWalletDetailsWithUserId,
+} from "../controllers/wallet.controller";
+import {
+  validateCreateWallet,
+  validateGetWalletDetails,
+} from "../middlewares/validations.middleware";
 
 const walletRouter = Router();
 
@@ -12,6 +18,13 @@ walletRouter.post(
   passport.authenticate("jwt", { session: false }),
   validate(validateCreateWallet, {}, {}),
   createWallet
+);
+
+walletRouter.get(
+  "/user/:userId",
+  passport.authenticate("jwt", { session: false }),
+  validate(validateGetWalletDetails, {}, {}),
+  getWalletDetailsWithUserId
 );
 
 export default walletRouter;

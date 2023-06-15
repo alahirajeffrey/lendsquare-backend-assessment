e7d0d52ae8f7f979f9ea25b6d83b099eef581698
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-15 10:09:50 +0100

    added route to get wallet transactions

--- src/routes/transaction.route.ts
+++ src/routes/transaction.route.ts
@@ -4,21 +4,30 @@ import passport from "passport";
 import "../config/passport.config";
 import {
   fundAccount,
+  getWalletTransactions,
   transferFunds,
   withdrawal,
 } from "../controllers/transactions.controller";
 import {
   validateFundAccount,
+  validateGetWalletTransactions,
   validateTransfer,
   validateWithdrawal,
 } from "../middlewares/validations.middleware";
 
 const transactionRouter = Router();
 
+transactionRouter.get(
+  "/wallet/:walletId",
+  passport.authenticate("jwt", { session: false }),
+  validate(validateGetWalletTransactions, {}, {}),
+  getWalletTransactions
+);
+
 transactionRouter.patch(
   "/fund-account/:walletId",
   passport.authenticate("jwt", { session: false }),
-  validate(validateFundAccount, {}, {}),
+  // validate(validateGetWalletTransactions, {}, {}),
   fundAccount
 );
 

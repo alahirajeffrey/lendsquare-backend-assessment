eff374fa545b9a064cdedb831837f05e7493c409
alahirajeffrey <alahirajeffrey@gmail.com>
2023-06-18 12:30:12 +0100

    setup auth test to use in memory sqlite3 db

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -1,13 +1,16 @@
 import request from "supertest";
 import server from "../src/server";
-import db from "../src/knexfile";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import * as authControllers from "../src/controllers/auth.controller";
-import { ApiResponse } from "../src/types/response.type";
-import { Response } from "express";
+import { setupTestDatabase, teardownTestDatabase } from "./test.setup";
 
 describe("Auth test", () => {
+  beforeAll(async () => {
+    await setupTestDatabase();
+  });
+
+  afterAll(async () => {
+    await teardownTestDatabase();
+  });
+
   describe("POST /api/v1/auth/register", () => {
     it("should register a new user and return a 201 response", async () => {
       const response = await request(server)
@@ -36,40 +39,39 @@ describe("Auth test", () => {
       expect(response.body).toHaveProperty("message");
     });
   });
-});
 
-describe("Login test", () => {
-  describe("POST /api/v1/auth/login", () => {
-    it("should login user and return a 200 response", async () => {
-      await request(server)
-        .post("/api/v1/auth/login")
-        .send({
-          email: "johndoe@email.com",
+  describe("Login test", () => {
+    describe("POST /api/v1/auth/login", () => {
+      it("should login user and return a 200 response", async () => {
+        await request(server)
+          .post("/api/v1/auth/login")
+          .send({
+            email: "johndoe@email.com",
+            password: "passworded",
+          })
+          .expect(200);
+      });
+
+      it("should return a 401 error if password is incorrect", async () => {
+        const response = await request(server)
+          .post("/api/v1/auth/login")
+          .send({
+            email: "johndoe@email.com",
+            password: "passworded2",
+          })
+          .expect(401);
+        expect(response.body).toHaveProperty("message", "incorrect password");
+      });
+
+      it("should return a 404 error if user does not exist", async () => {
+        const response = await request(server).post("/api/v1/auth/login").send({
+          email: "john@email.com",
           password: "passworded",
-        })
-        .expect(200);
-    });
-
-    it("should return a 401 error if password is incorrect", async () => {
-      const response = await request(server)
-        .post("/api/v1/auth/login")
-        .send({
-          email: "johndoe@email.com",
-          password: "passworded2",
-        })
-        .expect(401);
-      expect(response.body).toHaveProperty("message", "incorrect password");
-    });
-
-    it("should return a 404 error if user does not exist", async () => {
-      const response = await request(server)
-        .post("/api/v1/auth/login")
-        .send({
-          email: "johndoe@gmail.com",
-          password: "passworded2",
-        })
-        .expect(404);
-      expect(response.body).toHaveProperty("message", "user does not exist");
+        });
+        // .expect(404);
+        expect(response.statusCode == 404);
+        expect(response.body).toHaveProperty("message", "user does not exist");
+      });
     });
   });
 });
